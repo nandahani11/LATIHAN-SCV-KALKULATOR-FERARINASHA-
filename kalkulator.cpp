3e05bcd5695dc9cf4b5c18555f61b14895cc5594
ferhatmuhyasin <62926532+ferhatmuhyasin@users.noreply.github.com>
2020-04-01 16:47:12 +0700

    Method-kali.cpp

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -7,3 +7,17 @@ class kalkulator{
       void_kali();
       void_bagi();
 }
+
+    //perkalian
+    void kali()
+    {
+        int x, y;
+        cout << "Masukkan Nilai X  : ";
+        cin >> x;
+        cout << "Masukkan Nilai Y  : ";
+        cin >> y;
+        cout << "Hasil Pertambahan : " << x * y << endl;
+    }
+
+main(){
+}
